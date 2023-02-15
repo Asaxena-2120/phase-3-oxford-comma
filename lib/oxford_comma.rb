bbea9a0083ee67a63b7f48aa017a13a79d4d227c
Aastha <saxena.aastha91@gmail.com>
2023-02-15 21:17:10 +0530

    commit tree ee0e0fd12e2ae4b761e4533c3fe08b6012c8484d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+    if array.length==1
+        return array[0]
+    elsif array.length==2
+        return array[0] +" and "+array[1]
+    else
+        arr1=array[0,array.length-1].join(", ")
+        arr2=arr1+", and "+array.last
+        return arr2
+    end
 
 end
